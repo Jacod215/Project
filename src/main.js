ef50a0f9d9014dceb68267e55cbe0be0d5128feb
Jacod215 <andrzejszewczenko27.08.2007@gmail.com>
2025-06-10 09:43:20 +0200

    Initial commit: CV website with responsive design

---
+++ src/main.js
@@ -0,0 +1,4 @@
+// Initialize mobile menu toggle
+document.querySelector('.nav__toggle').addEventListener('click', () => {
+    document.querySelector('.nav__menu').classList.toggle('nav__menu--active');
+});
